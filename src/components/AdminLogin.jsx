53065614316c188d62f4a979e5849604fbf6b021
Daniel-Collado <danielcollado82@gmail.com>
2025-12-20 12:22:01 -0300

    robots.txt agregado

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,14 +1,26 @@
 // src/components/AdminLogin.jsx
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase'; // Importa la instancia de auth
+import { auth } from '../firebase'; 
 import './Admin.css';
 
 const AdminLogin = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+
+    
+    useEffect(() => {
+        const metaRobots = document.createElement('meta');
+        metaRobots.name = "robots";
+        metaRobots.content = "noindex, nofollow";
+        document.head.appendChild(metaRobots);
+
+        return () => {
+            document.head.removeChild(metaRobots);
+        };
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,12 +29,7 @@ const AdminLogin = () => {
         try {
             await signInWithEmailAndPassword(auth, email, password);
             
-            // Paso 2: Si llegamos aquí, el login fue exitoso.
-            console.log('Login exitoso.');
-
         } catch (authError) {
-            // Paso 3: Si hay un error, lo registramos.
-            // Esto es crucial para ver si hay un error de conexión, de credenciales o de otro tipo.
             console.error('Error de autenticación:', authError);
             
             if (authError.code === 'auth/wrong-password' || authError.code === 'auth/user-not-found') {
