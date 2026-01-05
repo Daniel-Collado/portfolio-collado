6c00270537ed020a5c4af0d9aee2a4434b1a28b0
Daniel-Collado <danielcollado82@gmail.com>
2026-01-04 23:43:25 -0300

    aria label en boton config

--- src/components/Settings/SettingsButton.jsx
+++ src/components/Settings/SettingsButton.jsx
@@ -2,7 +2,7 @@ import { FaCog } from "react-icons/fa";
 
 export default function SettingsButton({ onClick }) {
     return (
-        <button className="settings-button" onClick={onClick}>
+        <button className="settings-button" aria-label="Abrir configuración" onClick={onClick}>
         <FaCog size={18} />
         </button>
     );
